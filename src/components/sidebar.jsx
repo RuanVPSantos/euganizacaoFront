acb19b0766f633d8758124e713a1ab9ec4916e1c
Ruan Santos <88584184+RuanVPSantos@users.noreply.github.com>
2024-04-19 15:24:21 -0300

    links

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -17,10 +17,11 @@ import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 const FraterLinks = [
   { text: 'Caju Limão', href: 'https://plataforma.vobi.com.br/profissional/projetos/perfil/174872/financeiro/cobrancas' },
+  { text: 'QI 05 Casa 10', href: 'https://plataforma.vobi.com.br/profissional/projetos/perfil/188714/financeiro/cobrancas' },
+  { text: 'Bar Superquadra', href: 'https://plataforma.vobi.com.br/profissional/projetos/perfil/198694/financeiro/cobrancas' },
   { text: 'QI 11 - Rafael', href: 'https://plataforma.vobi.com.br/profissional/projetos/perfil/170234/financeiro/cobrancas' },
   { text: 'Hênio', href: 'https://plataforma.vobi.com.br/profissional/projetos/perfil/132277/financeiro/cobrancas' },
   { text: 'Tiffany', href: 'https://plataforma.vobi.com.br/profissional/projetos/perfil/185454/financeiro/cobrancas' },
-  { text: 'QI 11 Casa 10', href: '' },
   { text: '402 Norte', href: 'https://plataforma.vobi.com.br/profissional/projetos/perfil/147823/financeiro/cobrancas' },
   { text: 'Darson', href: 'https://plataforma.vobi.com.br/profissional/projetos/perfil/132283/financeiro/cobrancas' },
   { text: 'Maia', href: '' },
